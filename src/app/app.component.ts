89cdb1316c2648cbd896c4aba0b94f52bdee20df
Juan Stampone <juanstampone@gmail.com>
2019-08-16 09:48:35 -0300

    Se agrego idInstr y mejoraron botones de ciclos de cada instruccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,15 @@ export class AppComponent implements OnInit {
   
   title = 'SimuladorRob';
   instrucciones;
-  tipoInstrucciones:string[] = ["ADD","SUB","MUL","DIV","ST","LD"];
+  tipoInstrucciones = [{ type: "ADD", ciclo: 1},
+                        {type: "SUB", ciclo: 1},
+                        {type:"MUL", ciclo:1},
+                        {type:"DIV", ciclo:1},
+                        {type:"ST", ciclo:1},
+                        {type:"LD",ciclo: 1}];
   nombreRegistro:string[] = ["R1","R2","R3","R4","R5","R6","R7","R8","R9","R10"];
   numOrden = 1;
-
+  idInstruccion = 0;
   btnDefaultIns = {
     type : "INSTRUCCION",
     dst: "DST",
@@ -25,22 +30,32 @@ export class AppComponent implements OnInit {
     op2: "OP2"
   };
 
-
+  
   constructor(public dataInstruccion: Instruccion) { }
 
   ngOnInit() {
-    this.instrucciones = this.dataInstruccion.getInstrucciones();    
+    this.instrucciones = this.dataInstruccion.getInstrucciones();
+    this.idInstruccion = this.dataInstruccion.getInstrucciones().length; 
   }
-
-  change(pos,name){
+  
+  cambiar(pos,name){
     this.btnDefaultIns[pos] = name;
   }
 
   agregarInstruccion(){ 
-    let instruccion:{id,tipo,destino,op1,op2} = {id: 1, tipo: this.btnDefaultIns.type , destino: this.btnDefaultIns.dst, op1:this.btnDefaultIns.op1, op2:this.btnDefaultIns.op2};
+    this.idInstruccion++;
+    let instruccion:{id,tipo,destino,op1,op2} = {id: this.idInstruccion, tipo: this.btnDefaultIns.type , destino: this.btnDefaultIns.dst, op1:this.btnDefaultIns.op1, op2:this.btnDefaultIns.op2};
     this.dataInstruccion.crearInstrucciones(instruccion);
+
 }
-    cambiarOrden(num){
+  cambiarOrden(num){
       this.numOrden = num;
+  }
+  cambiarCiclo(pos,numCiclo){
+    for (let tipoIns of this.tipoInstrucciones) {
+        if (tipoIns.type == pos )
+          tipoIns.ciclo = numCiclo;
     }
+  }
+
 }
