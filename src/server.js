cb0415f6e0769b056555930aa0b1280ac5165eb4
Juan Stampone <juanstampone@gmail.com>
2019-09-13 00:13:49 -0300

    Initial herokuapp

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/SimuladorRob'));
+app.use(express.static(__dirname + '/dist/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/SimuladorRob/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
+
+console.log("Correcto");
