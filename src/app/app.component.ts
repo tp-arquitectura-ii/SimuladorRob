1d30872feed13e4618a70516aff329e30441c522
Juan Stampone <juanstampone@gmail.com>
2019-10-14 18:06:39 -0300

    Version Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,7 @@ export class AppComponent implements OnInit {
     ];
 
     registers:string[] = [
+      "R0",
       "R1",
       "R2",
       "R3",
@@ -42,7 +43,28 @@ export class AppComponent implements OnInit {
       "R7",
       "R8",
       "R9",
-      "R10"
+      "R10",
+      "R11",
+      "R12",
+      "R13",
+      "R14",
+      "R15",
+      "R16",
+      "R17",
+      "R18",
+      "R19",
+      "R20",
+      "R21",
+      "R22",
+      "R23",
+      "R24",
+      "R25",
+      "R26",
+      "R27",
+      "R28",
+      "R29",
+      "R30",
+      "R31",
     ];
     
     DHeaders: Array<string>;
